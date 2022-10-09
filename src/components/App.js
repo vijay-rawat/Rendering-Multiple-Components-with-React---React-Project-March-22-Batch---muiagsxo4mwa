916077fd0f919c0e7504cbcae5d3bfdec45bf596
Vijay Rawat <30477642+vijay-rawat@users.noreply.github.com>
2022-10-09 19:52:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">Photo Gallery Application</h1>
+      <p data-ns-test="project-description">
+        I want to build Photo Gallery Application in React
+      </p>
+    </div>
+  );
+};
 
 export default App;
+
